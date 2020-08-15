393300d9195b9facb1811b5929623d3aa63f90aa
Gustavo Ferreira <Gustav-F@fgustavo016@gmail.com>
2020-08-15 19:27:01 -0300

    Revert "Adicionada versão não finalizada do projeto"
    This reverts commit f33cfadc23fd39ff5f7c54e50aec360707593e47.

--- Kindle - Etapa 3/SELECT Livro.sql
+++
@@ -1,5 +0,0 @@
-SELECT * 
-FROM livro AS l
-INNER JOIN editora ON id_pessoa = id_editora
-INNER JOIN genero_livro AS gl ON gl.id_livro = l.id_livro
-INNER JOIN escritor_livro AS el ON el.id_livro = l.id_livro;
--- Kindle - Etapa 3/src/module-info.java
+++
@@ -1,3 +0,0 @@
-module Kindle {
-	requires java.sql;
-}
